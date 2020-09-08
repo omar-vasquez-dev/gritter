8622d05a456c1ebd412079ed0bf04b838b8b5b28
omar-vasquez-dev <omar.vasquez.dev@gmail.com>
2020-09-07 21:55:15 -0500

    first commit -m

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Hello world</Button>
+  </div>
+);
+
+export default App;
